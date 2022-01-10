66a2875e400ebe4bb44e883a9ccec92281ffdf18
brianA1998 <brian.alaniz@mercadolibre.com>
2022-01-10 00:54:10 -0300

    **ADDED** - Separation of Concerns with clean architecture - Create data class Coin - Create resource how sealed class

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinPaprikaApi.kt
@@ -0,0 +1,10 @@
+package com.plcoding.cryptocurrencyappyt.data.remote
+
+import retrofit2.http.GET
+
+interface CoinPaprikaApi {
+
+    @GET("/v1/coins")
+    suspend fun getCoins()
+
+}
---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/dto/CoinDto.kt
@@ -0,0 +1,13 @@
+package com.plcoding.cryptocurrencyappyt.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class CoinDto(
+    val id: String,
+    @SerializedName("is_active") val isActive: Boolean,
+    @SerializedName("is_name") val isNew: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+    val type: String
+)
